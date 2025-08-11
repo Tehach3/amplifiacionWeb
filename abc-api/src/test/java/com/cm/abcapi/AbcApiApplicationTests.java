efff52c7a77425ddd362a7e779a39c0c16bd752f
Martin <tehach3@gmail.com>
2025-08-11 01:22:34 -0300

    initial commit

---
+++ abc-api/src/test/java/com/cm/abcapi/AbcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.abcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
